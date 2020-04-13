8ec5479f6f878f3577a9c62d1c284b824fe4a894
6035512066 <pdtry5698@gmail.com>
2020-04-13 22:25:41 +0700

    edit

--- src/component/Login.js
+++ src/component/Login.js
@@ -6,36 +6,40 @@ import "mdbreact/dist/css/mdb.css";
 
 class Login extends Component {
 
-    constructor(props){
-        
+    constructor(props) {
+
+        super(props)
         this.state = {
-            
+
             email: "",
             password: ""
+
         }
+
+
     }
 
-    renter(){
-return (
-<MDBContainer>
-  <MDBRow>
-    <MDBCol md="6">
-      <form>
-        <p className="h5 text-center mb-4">Sign in</p>
-        <div className="grey-text">
-          <MDBInput label="Type your email" icon="envelope" group type="email" validate error="wrong"
-            success="right" value={this.state.email} />
-          <MDBInput label="Type your password" icon="lock" group type="password" validate />
-        </div>
-        <div className="text-center">
-          <MDBBtn>Login</MDBBtn>
-        </div>
-      </form>
-    </MDBCol>
-  </MDBRow>
-</MDBContainer>
-);
-}
+    render() {
+        return (
+            <MDBContainer>
+                <MDBRow>
+                    <MDBCol md="6">
+                        <form>
+                            <p className="h5 text-center mb-4">Sign in</p>
+                            <div className="grey-text">
+                                <MDBInput label="Type your email" icon="envelope" group type="email" validate error="wrong"
+                                    success="right" value={this.state.email} />
+                                <MDBInput label="Type your password" icon="lock" group type="password" validate value={this.state.password} />
+                            </div>
+                            <div className="text-center">
+                                <MDBBtn>Login</MDBBtn>
+                            </div>
+                        </form>
+                    </MDBCol>
+                </MDBRow>
+            </MDBContainer>
+        );
+    }
 }
 
 export default Login;
