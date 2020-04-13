1e02794ffc40e4e94f843239810472c839520c43
6035512066 <pdtry5698@gmail.com>
2020-04-13 23:15:21 +0700

    add sigup

--- src/component/Login.js
+++ src/component/Login.js
@@ -10,6 +10,9 @@ class Login extends Component {
     constructor(props) {
 
         super(props)
+        this.login = this.login.bind(this)
+        this.handleChange = this.handleChange.bind(this)
+        this.sigup = this.sigup.bind(this)
         this.state = {
 
             email: "",
@@ -20,11 +23,11 @@ class Login extends Component {
     }
 
 
-    Login(e) {
+    login(e) {
 
         e.preventDefault()
 
-        fire.auth().signInWithEmailAndPassword(this.state.email,this.state.passeword).then((u) =>{
+        fire.auth().signInWithEmailAndPassword(this.state.email, this.state.passeword).then((u) => {
             console.log(u)
         }).catch((err) => {
             console.log(err)
@@ -32,9 +35,18 @@ class Login extends Component {
 
     }
 
-    handleChange(){
+    handleChange(e) {
         this.setState({
-            [e.target.nane] : e.target.value
+            [e.target.name]: e.target.value
+        })
+    }
+
+    sigup(e){
+        e.preventDefault()
+        fire.auth().createUserWithEmailAndPassword(this.state.email, this.state.password).then((u) => {
+            console.log(u)
+        }).catch((err) =>{
+            console.log(err)
         })
     }
 
@@ -49,12 +61,13 @@ class Login extends Component {
                         <form>
                             <p className="h5 text-center mb-4">Sign in</p>
                             <div className="grey-text">
-                                <MDBInput label="Type your email" icon="envelope" group type="email" validate error="wrong"
-                                    success="right" value={this.state.email} />
-                                <MDBInput label="Type your password" icon="lock" group type="password" validate value={this.state.password} />
+                                <MDBInput label="Type your email" icon="envelope" group type="email" name="email" validate error="wrong"
+                                    success="right" value={this.state.email} onChange={this.handleChange} />
+                                <MDBInput label="Type your password" icon="lock" group type="password" name="password" validate value={this.state.password} onChange={this.handleChange} />
                             </div>
                             <div className="text-center">
-                                <MDBBtn>Login</MDBBtn>
+                                <MDBBtn color="primary" onClick={this.login}>Login</MDBBtn>
+                                <MDBBtn color="primary" onClick={this.sigup}>Register</MDBBtn>
                             </div>
                         </form>
                     </MDBCol>
